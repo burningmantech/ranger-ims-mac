9756683c25f961aa0fbe334ad9498b31bb01b2cb
Wilfredo Sánchez <wsanchez@wsanchez.net>
2015-07-04 23:36:16 -0600

    Add alert()

---
+++ Incidents/Alert.swift
@@ -0,0 +1,24 @@
+//
+//  Alert.swift
+//  Incidents
+//
+//  © 2015 Burning Man and its contributors. All rights reserved.
+//  See the file COPYRIGHT.md for terms.
+//
+
+import Cocoa
+
+
+
+func alert(title title: String = "Alert", message: String = "") {
+    logInfo(message)
+    
+    let alert = NSAlert()
+
+    alert.alertStyle = NSAlertStyle.InformationalAlertStyle
+    alert.showsHelp = false
+    alert.messageText = title
+    alert.informativeText = message
+
+    alert.runModal()
+}
