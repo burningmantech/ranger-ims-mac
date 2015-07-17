1cab1e8cd18c59ae5be8bae4cb756de5ded701e7
Wilfredo Sánchez <wsanchez@wsanchez.net>
2015-07-17 13:48:56 -0700

    Complete location names.

---
+++ Incidents/LocationNameFieldDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  LocationNameFieldDelegate.swift
+//  Incidents
+//
+//  Created by Wilfredo Sánchez Vega on 7/17/15.
+//  Copyright © 2015 Burning Man. All rights reserved.
+//
+
+import Foundation
