0b58df5a7af8adcc3da32e200882f709f355b39d
Wilfredo Sanchez Vega <wsanchez@apple.com>
2016-02-23 18:15:49 -0800

    Test description(), cleanup

--- IncidentsTests/DateTimeTests.swift
+++ IncidentsTests/DateTimeTests.swift
@@ -11,9 +11,13 @@ import XCTest
 
 
 
-class DateTimeComparisonTests: XCTestCase {
+class DateTimeRFC3339Tests: XCTestCase {
 
-    func test_fromRFC3339String_asRFC3339String() {
+    // FIXME: Can't test fromRFC3339String because we can't access the internal
+    // NSDate value, which is private.
+    
+
+    func test_fromRFC3339String_asRFC3339String_roundtrip() {
         XCTAssertEqual(
             DateTime.fromRFC3339String(date1String).asRFC3339String(),
             date1String
@@ -21,22 +25,46 @@ class DateTimeComparisonTests: XCTestCase {
     }
 
 
-// FIXME: Figure out how to set current locale?
-//    func test_fromRFC3339String_asShortString() {
+    func test_description() {
+        XCTAssertEqual(
+            DateTime.fromRFC3339String(date1String).description,
+            date1String
+        )
+    }
+
+    
+//    // FIXME: Figure out how to set current locale so that the tests below work
+//    // reliably.
+//
+//    
+//    func test_asShortString() {
 //        XCTAssertEqual(
 //            DateTime.fromRFC3339String(date1String).asShortString(),
-//            "xxx"
+//            "20/08:20"
+//        )
+//    }
+//    
+//
+//    func test_asString() {
+//        XCTAssertEqual(
+//            DateTime.fromRFC3339String(date1String).asString(),
+//            "1971-04-20 08:20"
 //        )
 //    }
 //
 //
-//    func test_fromRFC3339String_asLongString() {
+//    func test_asLongString() {
 //        XCTAssertEqual(
 //            DateTime.fromRFC3339String(date1String).asLongString(),
-//            "xxx"
+//            "Tuesday, April 20, 1971 08:20:04 PST"
 //        )
 //    }
 
+}
+
+
+
+class DateTimeComparisonTests: XCTestCase {
 
     func test_equal() {
         XCTAssertEqual(
